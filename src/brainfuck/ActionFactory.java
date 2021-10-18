3d941b6d34c6f5657217b77ad0bd97ccd5138395
Bogdan <50446763+bogdanvlas2000@users.noreply.github.com>
2021-10-18 22:58:53 +0300

    add .gitignore

--- src/brainfuck/ActionFactory.java
+++ src/brainfuck/ActionFactory.java
@@ -29,30 +29,4 @@ public class ActionFactory {
         };
         return action;
     }
-
-    //СПОСОБ 2
-    //реализация на лямбда-выражениях
-    //в этом случае можно удалить всю иерархию Action
-    public Consumer<Context> getActionWithLambdas(char ch) {
-        Consumer<Context> action = switch (ch) {
-            case '>' -> ctx -> {
-                if (ctx.hasNextCell()) {
-                    ctx.nextCell();
-                }
-            };
-            case '<' -> ctx -> {
-                if (ctx.hasPrevCell()) {
-                    ctx.prevCell();
-                }
-            };
-            case '+' -> ctx -> ctx.increase();
-            case '-' -> ctx -> ctx.reduce();
-            case '.' -> ctx -> ctx.read();
-            case ',' -> ctx -> ctx.write();
-            case '[' -> ctx -> ctx.processCycleBegin();
-            case ']' -> ctx -> ctx.processCycleEnd();
-            default -> throw new WrongInputCharacterException(ch);
-        };
-        return action;
-    }
 }
